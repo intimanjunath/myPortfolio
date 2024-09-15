14aec55fa54cf5a304eceb2cd8772e1ddbb3a362
intimanjunath <intimanjunath@gmail.com>
2024-09-15 01:03:49 -0700

    modified code

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,23 +8,22 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Manjunatha Inti </span>
+            from <span className="purple"> San Jose, California.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I'm a student at San Jose State University, Pursuing my masters in Software Engineering.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have completed Bachelor's in Computer Science and Engineering from Sathyabama University, Chennai.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Playing Games - Valorant and FIFA
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Swimming
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
@@ -34,7 +33,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Inti</footer>
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -8,7 +8,7 @@ function Type() {
         strings: [
           "Software Developer",
           "Freelancer",
-          "MERN Stack Developer",
+          "Machine Learning Developer",
           "Open Source Contributor",
         ],
         autoStart: true,
